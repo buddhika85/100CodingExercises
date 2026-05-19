416dff889dd2ce3b72bfb15f53384090a86bf5b1
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2026-05-20 03:54:24 +1000

    char freq 3

---
+++ CodingExercises/CharFreq.cs
@@ -0,0 +1,17 @@
+﻿namespace CodingExercises;
+
+public class CharFreq
+{
+    public static Dictionary<char, int> CountCharacterFrequencies(
+        string input)
+    {
+        return (from x in input
+            group x by x
+            into charGroup
+            select new
+            {
+                Character = charGroup.Key,
+                Count = charGroup.Count()
+            }).ToDictionary(k => k.Character, v => v.Count);
+    }
+}
